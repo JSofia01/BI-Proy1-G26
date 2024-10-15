7b1757c8fdf1454a44174f9fe56ec012c60db183
Juliana <j.ahumadaa@uniandes.edu.co>
2024-10-14 20:33:03 -0500

    cambios

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import cross_origin, CORS
 import joblib
 import pandas as pd
 from sklearn.metrics import classification_report, accuracy_score
@@ -7,7 +7,7 @@ from auto_modelo import TextPreprocessing
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
 
 modelo = joblib.load('modelo_svm.pkl')
 
@@ -30,4 +30,4 @@ def predict():
         return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ async function makePrediction() {
         return;
     }
 
-    const response = await fetch('http://localhost:5000/predict', {
+    const response = await fetch('http://127.0.0.1:5000/predict', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
